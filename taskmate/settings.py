d31211a674471d87aa17a5aea1cbba5a29f4d1eb
eltinawh <eltinawh@yahoo.com>
2024-09-12 21:56:31 +0700

    whitenoise setting

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -44,9 +44,10 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
 ]
-
+...
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+...
 ROOT_URLCONF = 'taskmate.urls'
 
 TEMPLATES = [
